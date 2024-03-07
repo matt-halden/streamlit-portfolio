d3c066f2518f9099d56d61a333ef5f1956bc15a1
matt <mjahalden@gmail.com>
2024-03-07 09:43:07 -0700

    Rendered titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # set layout of page/format it correctly
 st.set_page_config(layout="wide")
@@ -17,3 +18,18 @@ with col2:
     I like dogs.
     """
     st.info(content)
+
+st.write("Below is our apps you can find")
+
+# col 3, 4 will be the two columns under the first row!
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df.iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df.iterrows():
+        st.header(row["title"])
