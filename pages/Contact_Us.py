8c93da166c3ae4a76139f7d00645419915005802
matt <mjahalden@gmail.com>
2024-04-01 10:28:16 -0700

    added dropdown function on Contact Us page

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from send_email import send_email
+import pandas as pd
 
 st.header("Contact Me")
 
+df = pd.read_csv("topics.csv")
+#select_options = ["Option 1", "Option 2", "Option 3"]
+
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email")
+    select_box = st.selectbox("What topic?", df)
     raw_message = st.text_area("You Message")
-    message = =f"""\
+    message = f"""\
     Subject: New email from {user_email}
     From: {user_email}
+    Topic {select_box}
     {raw_message}
 """
     button = st.form_submit_button("Submit")
